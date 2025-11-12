48ab2458b61106481268e0495aac3eb7c7848540
Natrix23 <natrix.n23@gmail.com>
2025-11-13 00:21:55 +0100

    -

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,8 +1,10 @@
-import Script from 'next/script'
-import * as gtag from '@/lib/gtag'
+'use client' // Lägg till denna rad högst upp
+import './globals.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { useState } from 'react'
+import Script from 'next/script'
+import * as gtag from '@/lib/gtag'  
 
 export default function RootLayout({
   children,
